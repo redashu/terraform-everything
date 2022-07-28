f27a0a290568d2fce5af5a6a50b6fb3d2ccad119
Ashutoshh <asashuthegreat4@gmail.com>
2022-07-28 17:31:43 +0530

    Add files via upload

---
+++ aws_datasources/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAQ4OUTXWCVXMO2UFZ"
+  secret_key = "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"
+}
+
+data "aws_ami" "image1" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+output "image-id" {
+  value = data.aws_ami.image1.id
+
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.image1.id
+  key_name      = "vodakey"
+  instance_type = "t2.micro"
+
+}
